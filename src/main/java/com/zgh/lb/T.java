d5fc9b5798d97977c482af69ecbfc2b3f8b35ffe
lenovo <285917033@qq.com>
2021-02-10 10:57:54 +0800

    java 8

---
+++ src/main/java/com/zgh/lb/T.java
@@ -0,0 +1,15 @@
+package com.zgh.lb;
+
+/**
+ * <Description> <br>
+ *
+ * @author 朱光和
+ * @version 1.0<br>
+ * @createDate 2021/02/10 9:44 <br>
+ */
+public class T {
+
+    public static void main(String[] args) {
+        System.out.println("abc");
+    }
+}
